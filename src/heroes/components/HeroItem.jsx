c63a10aefe5304a159bdd4dbe2b6c295e09521a7
Bryan Monge <mongesolanobryan@gmailcom>
2023-03-05 15:53:57 -0600

    Deleted "Ver más" link from HeroItem component.
    The link was deleted because it was not necessary to show it in the component, because all the component is clickable.

--- src/heroes/components/HeroItem.jsx
+++ src/heroes/components/HeroItem.jsx
@@ -12,41 +12,38 @@ export const HeroItem = ({
     return (
         <div className="col">
             <div className="card mb-3 shadow-sm animate__animated animate__fadeIn">
-                <div className="row g-0">
-                    <div className="col-5">
-                        <img
-                            src={heroImageUrl}
-                            className="img-fluid rounded-start"
-                            style={{ height: '100%', zIndex: '2' }}
-                            alt={superhero}
-                        />
-                    </div>
-                    <div className="col-7">
-                        <div
-                            className="h-100 d-flex flex-column justify-content-between"
-                            style={{ zIndex: '1' }}
-                        >
+                <Link to={`/hero/${id}`} className="stretched-link text-decoration-none">          
+                    <div className="row g-0">
+                        <div className="col-5">
+                            <img
+                                src={heroImageUrl}
+                                className="img-fluid rounded-start"
+                                style={{ height: '100%', zIndex: '2' }}
+                                alt={superhero}
+                            />
+                        </div>
+                        <div className="col-7">
                             <div
-                                className="card-body"
-                                style={{ height: 'fit-content' }}
+                                className="h-100 d-flex flex-column justify-content-between"
+                                style={{ zIndex: '1' }}
                             >
-                                <h3 className="card-title mb-1">{superhero}</h3>
-                                <h6 className="card-subtitle fw-semibold text-muted mb-2">
-                                    {alter_ego}
-                                </h6>
-                                <Link
-                                    to={`/hero/${id}`}
-                                    className="stretched-link"
+                                <div
+                                    className="card-body"
+                                    style={{ height: 'fit-content' }}
                                 >
-                                    Ver más
-                                </Link>
-                            </div>
-                            <div className="card-footer text-muted">
-                                {first_appearance}
+                                    <h3 className="card-title mb-1">{superhero}</h3>
+                                    <h6 className="card-subtitle fw-semibold text-muted mb-2">
+                                        {alter_ego}
+                                    </h6>
+                                    
+                                </div>
+                                <div className="card-footer text-muted">
+                                    {first_appearance}
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                </Link>
             </div>
         </div>
     );
