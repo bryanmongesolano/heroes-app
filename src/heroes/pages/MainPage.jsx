4a97cbc07a2a6a71576c4f2d96556ac43d859d2b
Bryan Monge <mongesolanobryan@gmailcom>
2023-03-05 17:12:43 -0600

    New page created to show the main page of the heroes app
    Also updated the index of the "pages" folder to export this new page.
    And updated the HeroesRoutes to use this new page as the main page.

---
+++ src/heroes/pages/MainPage.jsx
@@ -0,0 +1,12 @@
+import { HeroList } from "../components/HeroList";
+
+export const MainPage = () => {
+  return (
+    <>
+            <h1>All Heroes</h1>
+            <hr />
+            
+            <HeroList />
+        </>
+  )
+}
--- src/heroes/routes/HeroesRoutes.jsx
+++ src/heroes/routes/HeroesRoutes.jsx
@@ -1,6 +1,6 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { Navbar } from '../../ui/components';
-import { DCPage, HeroPage, MarvelPage, SearchPage } from '../pages';
+import { DCPage, HeroPage, MarvelPage, SearchPage, MainPage } from '../pages';
 
 export const HeroesRoutes = () => {
     return (
@@ -9,8 +9,9 @@ export const HeroesRoutes = () => {
 
             <div className="container my-4">
                 <Routes>
-                    <Route path="*" element={<Navigate to="/marvel" />} />
+                    <Route path="*" element={<Navigate to="/all" />} />
 
+                    <Route path="all" element={<MainPage />} />
                     <Route path="marvel" element={<MarvelPage />} />
                     <Route path="dc" element={<DCPage />} />
                     <Route path="search" element={<SearchPage />} />
